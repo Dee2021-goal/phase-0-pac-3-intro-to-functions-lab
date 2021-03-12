bcb8dfd5647206a4a806dcc3d0ea269fa29dd373
diane <dianeawoodward09@gmail.com>
2021-03-11 20:39:09 -0500

    completed assignment

--- test/root.js
+++ test/root.js
@@ -1,18 +1,13 @@
-global.expect = require('expect');
-
 const babel = require('babel-core');
 const jsdom = require('jsdom');
 const path = require('path');
-
 before(function(done) {
   const babelResult = babel.transformFileSync(
     path.resolve(__dirname, '..', 'index.js'), {
       presets: ['es2015']
     }
   );
-
   const html = path.resolve(__dirname, '..', 'index.html')
-
   jsdom.env(html, [], {
     src: babelResult.code,
     virtualConsole: jsdom.createVirtualConsole().sendTo(console)
@@ -20,11 +15,9 @@ before(function(done) {
     if (err) {
       return done(err);
     }
-
     Object.keys(window).forEach(key => {
       global[key] = window[key];
     });
-
     return done();
   });
 });
